f2d740948ad76a14e63a6ce73b5c27d6162df579
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-03-20 23:57:51 +0530

    Selenium First Steps - Driver and Browser Setup

--- src/test/java/stepDefinitions/Add_To_Cart_Steps.java
+++ src/test/java/stepDefinitions/Add_To_Cart_Steps.java
@@ -1,4 +1,33 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Add_To_Cart_Steps {
+    @Given("I am on the Store Page")
+    public void iAmOnTheStorePage() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://askomdch.com/store");
+
+    }
+
+    @When("I add a {string} to the Cart")
+    public void iAddAToTheCart(String arg0) {
+    }
+
+    @Then("I should see {int} {string} in the cart")
+    public void iShouldSeeInTheCart(int arg0, String arg1) {
+    }
 }
