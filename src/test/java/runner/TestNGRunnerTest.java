5e59edc4375c52452bb3386bfb28ced182bd59d5
Ankit-Keshri-QA <ankitkeshri198@gmail.com>
2025-04-10 20:20:43 +0530

    Section 26 - Cucumber Reporting in Project

--- src/test/java/runner/TestNGRunnerTest.java
+++ src/test/java/runner/TestNGRunnerTest.java
@@ -5,8 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        glue = {"stepDefinitions", "customType", "hooks", "factory","context","domainObjects","constants"},
-        features = {"src/test/resources/features"}
+        glue = {"stepDefinitions", "customType", "hooks", "factory", "context", "domainObjects", "constants"},
+        features = {"src/test/resources/features/add_cart.feature"},
+        plugin = {"html:target/cucumber-reports/cucumber.html"}
 )
 public class TestNGRunnerTest extends AbstractTestNGCucumberTests {
 
